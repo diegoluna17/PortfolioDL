d33d2be2700fdf091bae71b537899105d7f68d3a
Diego Luna <diegocheuquel17@hotmail.com>
2023-04-27 15:23:50 -0300

    Backend: Correccion Exp Lab

--- backend/src/main/java/com/portfoliodl/DL/Controller/CExperienciaLaboral.java
+++ backend/src/main/java/com/portfoliodl/DL/Controller/CExperienciaLaboral.java
@@ -31,7 +31,7 @@ import org.springframework.web.bind.annotation.PutMapping;
 @RestController
 @RequestMapping("explab")
 @CrossOrigin(origins = "http://localhost:4200")
-public class CExperienciaLaboral {
+public class cExperienciaLaboral {
     @Autowired
     sExperienciaLaboral sExperienciaL;
     
