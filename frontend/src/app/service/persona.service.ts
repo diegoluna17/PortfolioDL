82ac093959ff848aee62a44b5481e600746ee0e1
Diego Luna <diegocheuquel17@hotmail.com>
2023-04-27 15:35:20 -0300

    Frontend: Corrección Servicio Persona

--- frontend/src/app/service/persona.service.ts
+++ frontend/src/app/service/persona.service.ts
@@ -1,7 +1,8 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
-import { persona } from '../model/persona.model';
+import { Persona } from '../model/persona';
+
 
 @Injectable({
   providedIn: 'root'
@@ -9,9 +10,26 @@ import { persona } from '../model/persona.model';
 export class PersonaService {
   URL = 'http://localhost:8080/personas/';
 
-  constructor(private http: HttpClient) { }
+  constructor(private httpClient: HttpClient) { }
 
-  public getPersona(): Observable<persona>{
-    return this.http.get<persona>(this.URL + 'traer/perfil/1');    
+  public lista(): Observable<Persona[]>{
+    return this.httpClient.get<Persona[]>(this.URL + 'lista');
   }
+
+  public detail(id: number): Observable<Persona>{
+    return this.httpClient.get<Persona>(this.URL + `detail/${id}`)
+  }
+
+  public update(id: number, persona:Persona): Observable<any>{
+    return this.httpClient.put<any>(this.URL + `update/${id}`, persona);
+  }
+
+/*   public save(estudio:Persona): Observable<any>{
+    return this.httpClient.post<any>(this.URL + "create", estudio);  
+  }
+
+  public delete(id:number): Observable<any>{
+    return this.httpClient.delete<any>(this.URL + `delete/${id}`);
+  } */
+  
 }
