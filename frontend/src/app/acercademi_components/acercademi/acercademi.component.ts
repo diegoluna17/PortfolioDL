d6d9794b57c273ac64d6f41bb22e11d3b095948e
Diego Luna <diegocheuquel17@hotmail.com>
2023-04-27 15:36:20 -0300

    Frontend: Corrección Componente Acerca de Mi

--- frontend/src/app/acercademi_components/acercademi/acercademi.component.ts
+++ frontend/src/app/acercademi_components/acercademi/acercademi.component.ts
@@ -1,6 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-import { persona } from 'src/app/model/persona.model';
+import { ActivatedRoute } from '@angular/router';
+import { Persona } from 'src/app/model/persona';
 import { PersonaService } from 'src/app/service/persona.service';
+import { TokenService } from 'src/app/service/token.service';
+
+
 
 @Component({
   selector: 'app-acercademi',
@@ -8,12 +12,22 @@ import { PersonaService } from 'src/app/service/persona.service';
   styleUrls: ['./acercademi.component.css']
 })
 export class AcercademiComponent implements OnInit {
-  persona: persona = new persona("","",""); //Crea un objeto de tipo persona, clase del model
-
-  constructor(public personaService: PersonaService) { }
+  persona: Persona = new Persona('','','','')
+  isLogged = false
+  
+  constructor(public personaService: PersonaService, private tokenService: TokenService, private activateRouter: ActivatedRoute) { }
 
   ngOnInit(): void {
-    this.personaService.getPersona().subscribe(data=> {this.persona = data})
+    this.cargarPersona()
+    if(this.tokenService.getToken()){
+      this.isLogged = true;
+    } else{
+      this.isLogged = false;
+    }
+  }
+
+  cargarPersona(){
+   this.personaService.detail(1).subscribe(data=> {this.persona = data})
   }
 
 }
