708554dbc5c10b1bfd47e7052e7a68f30cfdf432
Diego Luna <diegocheuquel17@hotmail.com>
2023-04-27 15:43:32 -0300

    Frontend: Corrección Componente Proyectos

--- frontend/src/app/proyectos_components/proyectos/proyectos.component.ts
+++ frontend/src/app/proyectos_components/proyectos/proyectos.component.ts
@@ -1,15 +1,41 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Proyecto } from 'src/app/model/proyecto';
+import { SProyectosService } from 'src/app/service/s-proyectos.service';
+import { TokenService } from 'src/app/service/token.service';
 
 @Component({
   selector: 'app-proyectos',
   templateUrl: './proyectos.component.html',
   styleUrls: ['./proyectos.component.css']
 })
-export class ProyectosComponent implements OnInit {
+export class ProyectosComponent {
+  
+  proyectos: Proyecto[] = []
+  isLogged = false
 
-  constructor() { }
+  constructor(private sProyecto: SProyectosService, private tokenService: TokenService){}
 
-  ngOnInit(): void {
+  ngOnInit(): void{
+    this.cargaHyss()
+    if(this.tokenService.getToken()){
+      this.isLogged = true
+    } else {
+      this.isLogged = false
+    }
   }
 
+  cargaHyss(): void{
+    this.sProyecto.lista().subscribe(data => {this.proyectos = data})
+  }
+
+  delete(id: number){
+    if(id != undefined){
+      this.sProyecto.delete(id).subscribe(data => {
+        this.cargaHyss()
+        alert("Proyecto eliminado correctamente.")
+      }, err =>{
+        alert("No se pudo eliminar el proyecto.")
+      })
+    }
+  }
 }
